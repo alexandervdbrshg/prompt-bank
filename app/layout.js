18ed6a318cb575eefc80d8f7f2e361c31313654a
alexandervdbrshg <alexander.vandenbranden@sporthousegroup.com>
2026-01-14 15:21:40 +0100

    layout update

--- app/layout.js
+++ app/layout.js
@@ -3,18 +3,17 @@ import './globals.css'
 export const metadata = {
   title: 'Prompt Bank',
   description: 'Team collection of working prompts',
-  icons: {
-    icon: '/favicon.ico',
-  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+      </head>
       <body>{children}</body>
     </html>
   )
 }
-
-
-
