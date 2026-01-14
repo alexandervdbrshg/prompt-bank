011c747bd3c1a51adb4c7145dc99350730c1f28c
alexandervdbrshg <alexander.vandenbranden@sporthousegroup.com>
2026-01-14 14:18:58 +0100

    Update Next.js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+}
 
 module.exports = nextConfig
